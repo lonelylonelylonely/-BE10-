e154f23dbaf31ac44b4e55e0ccb901f83233428e
lonelylonelylonely <lonelylonelylonely@example.com>
2025-11-06 12:42:50 +0900

    5단계_리펙í토토링

---
+++ p-14063-1/src/main/java/com/back/App.java
@@ -0,0 +1,74 @@
+package com.back;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class App {
+    Scanner sc = new Scanner(System.in);
+    List<Wise_saying> list = new ArrayList<>();
+
+
+    // 시작
+    public void run() {
+        System.out.println("==  Wise_saying_app ==");
+
+        while (true) {
+            String command;
+            System.out.print("Command) ");
+            command = sc.nextLine().trim();
+
+            if (Objects.equals(command , "End")) {
+                break;
+
+            } else if (Objects.equals(command , "Register")) {
+                actionWrite();
+
+            } else if (Objects.equals(command , "List")) {
+                actionList();
+
+            }
+            // else if (Objects.equals(say.command, "Delete")) {  }
+
+        }
+
+    }
+
+    // 고객 응대 로직
+    private void actionWrite() {
+
+        System.out.print("Saying) ");
+        String content = sc.nextLine().trim();
+        System.out.print("Writer) ");
+        String author = sc.nextLine().trim();
+
+        write(content, author);
+
+        System.out.println("%d th saying is registered".formatted(list.size()));
+    }
+
+    private void actionList() {
+        System.out.println("id / author / content ");
+        System.out.println("________________________________");
+        for (Wise_saying i : list.reversed()) {
+            System.out.println(i.id + " / " +i.writer + " / " + i.saying);
+        }
+    }
+
+    private void write(String content, String author) {
+        Wise_saying say = new Wise_saying();
+        say.id = list.size() + 1;
+        say.saying = content;
+        say.writer = author;
+
+        list.add(say);
+    }
+
+    static class Wise_saying {
+        String saying;
+        String writer;
+        int id;
+    }
+}
+
--- p-14063-1/src/main/java/com/back/Main.java
+++ p-14063-1/src/main/java/com/back/Main.java
@@ -9,48 +9,64 @@ import java.util.Scanner;
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("==  Wise_saying_app ==");
-
-
-        Scanner sc = new Scanner(System.in);
-        List<Wise_saying> list = new ArrayList<>();
-
-        while (true) {
-
-
-            Wise_saying say = new Wise_saying();
-            System.out.print("Command) ");
-            say.command = sc.nextLine().trim();
-
-            if (Objects.equals(say.command, "End")) {
-                break;
-
-            } else if (Objects.equals(say.command, "Register")) {
-                say.id = list.size() + 1;
-                System.out.print("Saying) ");
-                say.saying = sc.nextLine().trim();
-                System.out.print("Writer) ");
-                say.writer = sc.nextLine().trim();
-                list.add(say);
-                System.out.println("%d th saying is registered".formatted(say.id));
-
-            } else if (Objects.equals(say.command, "List")) {
-                System.out.println("________________________________");
-                for (Wise_saying i : list.reversed()) {
-                    System.out.println(i.id + " / " +i.writer + " / " + i.saying);
-                }
-
-            }
-            // else if (Objects.equals(say.command, "Delete")) {  }
-
-        }
-
-    }
-
-    static class Wise_saying {
-        String command;
-        String saying;
-        String writer;
-        int id;
+        App app = new App();
+        app.run();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//        System.out.println("==  Wise_saying_app ==");
+//
+//
+//        Scanner sc = new Scanner(System.in);
+//        List<Wise_saying> list = new ArrayList<>();
+//
+//        while (true) {
+//
+//
+//            Wise_saying say = new Wise_saying();
+//            System.out.print("Command) ");
+//            say.command = sc.nextLine().trim();
+//
+//            if (Objects.equals(say.command, "End")) {
+//                break;
+//
+//            } else if (Objects.equals(say.command, "Register")) {
+//                say.id = list.size() + 1;
+//                System.out.print("Saying) ");
+//                say.saying = sc.nextLine().trim();
+//                System.out.print("Writer) ");
+//                say.writer = sc.nextLine().trim();
+//                list.add(say);
+//                System.out.println("%d th saying is registered".formatted(say.id));
+//
+//            } else if (Objects.equals(say.command, "List")) {
+//                System.out.println("________________________________");
+//                for (Wise_saying i : list.reversed()) {
+//                    System.out.println(i.id + " / " +i.writer + " / " + i.saying);
+//                }
+//
+//            }
+//            // else if (Objects.equals(say.command, "Delete")) {  }
+//
+//        }
+//
+//    }
+//
+//    static class Wise_saying {
+//        String command;
+//        String saying;
+//        String writer;
+//        int id;
+//    }
+
